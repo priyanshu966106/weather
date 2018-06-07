761b53b1b1e71cf7fff9b4dedd7ab22fae3a5879
Hardik Desai <hardik.desai@causecode.com>
2018-06-07 11:46:44 +0530

    added pic

--- src/components/FormPage.tsx
+++ src/components/FormPage.tsx
@@ -3,15 +3,18 @@ import {Button,Form} from 'react-bootstrap'
 export interface IFrontPageState {
     country: any;
     reps:any;
+    icon:''
 }
 
 
 export class FormPage extends React.Component<any,IFrontPageState>{
+ 
     constructor(props:any){
         super(props)
         this.state={
            country: "india",
-           reps:''
+           reps:'',
+           icon:''
         }
     }
   
@@ -24,13 +27,16 @@ showState=()=>{
   let cstring:string=this.state.country;
   const AppId:string="f7aa90d5e6f9b6337399b73f70e00b0a";
   const url:string=`http://api.openweathermap.org/data/2.5/weather?q=${cstring}&APPID=${AppId}`;
-  console.log('url',url);
+  console.log('url',url); 
   fetch(url,{
       method:'GET'
 
   }).then(response => response.json())
-     .then(json=>console.log('gotit',json)); 
+     .then(json=>this.setState({reps:json.weather[0].description,icon:json.weather[0].icon}));
+      
+    
 }
+
     public render(){
        
 return(
@@ -38,11 +44,12 @@ return(
 <div className="afpage">
     
     <Form>
-        <input type="text"  onChange={this.handleChange} />
+        <input type="text"  onChange={this.handleChange} placeholder="Enter Area" />
         <br/><br/>
         <Button bsStyle="primary"  onClick={this.showState}>
           Find weather
-        </Button>
+        </Button><br/><br/>
+        <img src={`http://openweathermap.org/img/w/${this.state.icon}.png`} />{this.state.reps}
     </Form>
  
 </div>
